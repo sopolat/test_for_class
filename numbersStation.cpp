3f036f3d9302e8f8be6f3d740e5e91e456a0f24f
sopolat <suleyman.olcay.polat@gmail.com>
2022-05-03 11:03:49 -0500

    add numberstains solution to the repo

---
+++ numbersStation.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+void answer(int l[],int t,int len, int* ans)
+{
+	int sum=l[0];
+	int i=0;
+	int j=0;
+	while(j<len)
+	{
+		if(sum==t)
+		{
+			ans[0]=i;
+			ans[1]=j;
+			return;
+		}
+		else if(sum>t)
+		{
+			sum-=l[i];
+			i++;
+		}
+		else
+		{
+			j++;
+			sum+=l[j];
+		}
+		cout<<i<<" , "<<j<<" , "<<sum<<endl;
+	}
+	return;
+}
+int main()
+{
+    int l[] ={4, 3, 10, 2, 8};
+	int len=5;
+	int t=12;
+	int ans[]={-1,-1};
+	answer(l,t,len,ans);
+	cout<<ans[0]<<" , "<<ans[1]<<endl;
+    return 0;
+}
